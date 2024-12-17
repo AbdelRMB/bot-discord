42c9b44f15ebb89fb487c0715f72b375df8a8d4c
Abdelrmb <richeabdelrahim1@gmail.com>
2024-12-17 23:01:59 +0100

    Add banlist command to display the list of banned users in the server

---
+++ commands/banlist.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('banlist')
+        .setDescription('Displays the list of banned users in the server'),
+    async execute(interaction) {
+        try {
+            const bans = await interaction.guild.bans.fetch();
+            const bannedUsers = bans.map(ban => ban.user.tag).join(', ');
+
+            if (!bannedUsers) {
+                await interaction.reply({ content: 'No banned users found.', ephemeral: true });
+            } else {
+                await interaction.reply({ content: `Banned users: ${bannedUsers}` });
+            }
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'An error occurred while fetching the ban list.', ephemeral: true });
+        }
+    }
+};
