a79537616796e06c04741ea7e8515c9e9557214f
Abdelrmb <richeabdelrahim1@gmail.com>
2024-12-17 21:33:53 +0100

    Initialize Discord bot project with basic configuration and dependencies

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const config = require('./config.json');
+
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => {
+    console.log(`Connecté en tant que ${client.user.tag}`);
+});
+
+client.login(config.token);
