e01b64257a4468b85d67438796d7f74fc964e123
Abdelrmb <richeabdelrahim1@gmail.com>
2024-12-17 22:37:53 +0100

    Refactor ban command for improved clarity and error handling

--- commands/ban.js
+++ commands/ban.js
@@ -3,31 +3,36 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ban')
-        .setDescription('Ban a member from a server.')
+        .setDescription('Ban a member from the server.')
         .addUserOption(option =>
-            option.setName('Member')
-                .setDescription('Member to ban.')
+            option.setName('user')
+                .setDescription('The user to ban')
                 .setRequired(true)
         )
         .addStringOption(option =>
             option.setName('reason')
-                .setDescription('Reason for the ban.')
+                .setDescription('The reason for the ban')
         ),
     async execute(interaction) {
-        const user = interaction.options.getUser('membre');
-        const reason = interaction.options.getString(' reason') || 'No reason provided.';
+        try {
+            const user = interaction.options.getUser('user');
+            const reason = interaction.options.getString('reason') || 'No reason specified';
 
-        const member = interaction.guild.members.cache.get(user.id);
+            const member = interaction.guild.members.cache.get(user.id);
 
-        if (!member) {
-            return interaction.reply({ content: 'This member is not on this server.', ephemeral: true });
+            if (!member) {
+                return interaction.reply({ content: 'This user is not in the server.', ephemeral: true });
+            }
+
+            if (!member.bannable) {
+                return interaction.reply({ content: 'I cannot ban this user.', ephemeral: true });
+            }
+
+            await member.ban({ reason });
+            await interaction.reply({ content: `User ${user.tag} has been banned. Reason: ${reason}` });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'An error occurred while trying to ban the user.', ephemeral: true });
         }
-
-        if (!member.bannable) {
-            return interaction.reply({ content: 'I cann\'t ban this member.', ephemeral: true });
-        }
-
-        await member.ban({ reason });
-        await interaction.reply({ content: `Member ${user.tag} is ban. Reason : ${reason}` });
     }
 };
