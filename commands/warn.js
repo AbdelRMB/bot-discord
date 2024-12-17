3d70afee5f1399bf607bc296eac5176dbcdcb4d7
Abdelrmb <richeabdelrahim1@gmail.com>
2024-12-17 23:02:12 +0100

    Add warn command to issue warnings to users in the server

---
+++ commands/warn.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('warn')
+        .setDescription('Warn a user in the server')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user to warn')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('The reason for the warning')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        try {
+            const user = interaction.options.getUser('user');
+            const reason = interaction.options.getString('reason');
+            const member = await interaction.guild.members.fetch(user.id);
+
+            await user.send(`You have been warned for: ${reason}`);
+
+            await interaction.reply({ content: `${user.tag} has been warned for: ${reason}` });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'An error occurred while warning the user.', ephemeral: true });
+        }
+    }
+};
