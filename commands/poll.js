7e6b1d1c3472db1a298a2066d0956553bf4cd1ca
Abdelrmb <richeabdelrahim1@gmail.com>
2024-12-17 23:02:06 +0100

    Add poll command to create a Yes/No poll with user interaction

---
+++ commands/poll.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('poll')
+        .setDescription('Create a poll with Yes/No options')
+        .addStringOption(option =>
+            option.setName('question')
+                .setDescription('The question for the poll')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        try {
+            const question = interaction.options.getString('question');
+
+            const pollEmbed = {
+                color: 0x00ff00,
+                title: 'Poll',
+                description: question,
+                fields: [
+                    { name: 'Yes', value: '👍', inline: true },
+                    { name: 'No', value: '👎', inline: true },
+                ],
+            };
+
+            const message = await interaction.reply({ embeds: [pollEmbed], fetchReply: true });
+            await message.react('👍');
+            await message.react('👎');
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'An error occurred while creating the poll.', ephemeral: true });
+        }
+    }
+};
