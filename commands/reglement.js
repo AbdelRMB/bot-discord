2df79b69bc8bb41a31bead95ba3e01097c418d4d
Abdelrmb <richeabdelrahim1@gmail.com>
2024-12-18 21:49:20 +0100

    Enhance interactive rules command to collect title and rule count with validation

--- commands/reglement.js
+++ commands/reglement.js
@@ -6,20 +6,26 @@ module.exports = {
         .setDescription('Creates an interactive set of rules.'),
     async execute(interaction) {
         try {
+            // Demande du titre
             await interaction.reply('What is the title of the rules?');
             
-            const filter = (response) => response.user.id === interaction.user.id;
+            // Filtre pour s'assurer que c'est bien l'utilisateur qui a lancé la commande qui répond
+            const filter = (response) => response.author.id === interaction.user.id;
+
+            // Collecte la première réponse (le titre des règles)
             const collector = interaction.channel.createMessageCollector({ filter, max: 1, time: 60000 });
 
             collector.on('collect', async (message) => {
                 const title = message.content;
                 await interaction.followUp(`The title is "${title}". How many rules are there? (Please respond with a number)`);
 
+                // Collecte la réponse concernant le nombre de règles
                 const ruleCountCollector = interaction.channel.createMessageCollector({ filter, max: 1, time: 60000 });
 
                 ruleCountCollector.on('collect', async (msg) => {
                     const ruleCount = parseInt(msg.content);
 
+                    // Validation du nombre de règles
                     if (isNaN(ruleCount) || ruleCount <= 0) {
                         await interaction.followUp('The number of rules must be a positive number. Command cancelled.');
                         return;
@@ -28,6 +34,7 @@ module.exports = {
                     const rules = [];
                     let currentRule = 1;
 
+                    // Fonction pour demander chaque règle
                     const askForRule = async () => {
                         await interaction.followUp(`What is rule ${currentRule}?`);
                         const ruleCollector = interaction.channel.createMessageCollector({ filter, max: 1, time: 60000 });
@@ -36,9 +43,11 @@ module.exports = {
                             rules.push(ruleMessage.content);
                             currentRule++;
 
+                            // Si nous avons encore des règles à recueillir
                             if (currentRule <= ruleCount) {
                                 askForRule();
                             } else {
+                                // Lorsque toutes les règles sont collectées, on affiche le résultat
                                 const embed = {
                                     color: 0x00ff00,
                                     title: title,
