b5df935259887330a7ee6113cff6867f0d2c29b7
Abdelrmb <richeabdelrahim1@gmail.com>
2024-12-28 10:07:52 +0100

    Enhance ticket creation command: validate channel type, update embed titles, and send embeds to the specified channel

--- commands/create-ticket.js
+++ commands/create-ticket.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, ActionRowBuilder, ButtonStyle, ButtonBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,13 +6,18 @@ module.exports = {
         .setDescription('Crée un système de tickets pour gérer les demandes utilisateurs')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     async execute(interaction) {
-        try{
+        try {
+            const channel = interaction.options.getChannel('channel');
+            if (!channel.isTextBased()) {
+                await interaction.reply({ content: 'The specified channel is not text-based.', ephemeral: true });
+                return;
+            }
 
             const ticketdevEmbed = new EmbedBuilder()
                 .setColor(0x00FF00)
-                .setTitle('Ticket Service')
+                .setTitle('Ticket client')
                 .setDescription('Choisissez le service demander');
-    
+
             const ticketdevButtons = new ActionRowBuilder()
                 .addComponents(
                     new ButtonBuilder()
@@ -24,11 +29,29 @@ module.exports = {
                         .setCustomId('ticket_dev_app')
                         .setLabel('Developpement Application')
                         .setStyle(ButtonStyle.Secondary)
-                        .setEmoji(':calling:')
+                        .setEmoji('📲') 
                 );
-    
-            await interaction.reply({ embeds: [ticketdevEmbed], components: [ticketdevButtons] });
-        } catch (error) { 
+
+            await channel.send({ embeds: [ticketdevEmbed], components: [ticketdevButtons] });
+
+            const ticketbugEmbed = new EmbedBuilder()
+                .setColor(0x00FF00)
+                .setTitle('Ticket Support')
+                .setDescription('Ouvrir un ticket Support.');
+
+            const ticketbugButtons = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('ticket_bug')
+                        .setLabel('Signaler un bug')
+                        .setStyle(ButtonStyle.Danger)
+                        .setEmoji('👨‍💻')  
+                );
+
+            await channel.send({ embeds: [ticketbugEmbed], components: [ticketbugButtons] });
+
+            await interaction.reply({ content: 'Panels créés !', ephemeral: true });
+        } catch (error) {
             console.error('Error while creating ticket:', error);
             await interaction.reply({ content: 'An error occurred while creating the ticket.', ephemeral: true });
         }
